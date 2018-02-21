678c18be35eb3b88bd1415e351de55b817bba3e4
deep <deep@improwised.com>
2018-02-21 11:42:20 +0530

    import module Getvalues

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Getvalues from './component/getvalue.js';
 
 class App extends Component {
   constructor(props) {
@@ -9,13 +10,6 @@ class App extends Component {
         total: 0,
         operator: ''
       };
-  }
-
-  getValue = (e) => {
-    const value = e.target.attributes.getNamedItem('value').value;
-    this.setState(() => {
-      return {current: value};
-    });
   }
 
   calculate = () => {
@@ -56,49 +50,63 @@ class App extends Component {
     });
   }
 
+   getValue = (e) => {
+    const value = e.target.attributes.getNamedItem('value').value;
+    this.setState(() => {
+      return {current: value};
+    });
+  }
+
   clear = () => {
-     this.setState({
-        total: 0,
-        current: 0,
-        operator: ''
-     })
+    this.setState({
+      total: 0,
+      current: 0,
+      operator: ''
+    });
   }
 
   render() {
     return (
       <div>
         <div>
-          <h3> {this.state.current} </h3>
-          <h2> {this.state.total} </h2>
-        </div>
-        <div>
-            <button value="7" onClick={this.getValue}> 7 </button>
-            <button value="8" onClick={this.getValue} > 8 </button>
-            <button value="9" onClick={this.getValue} > 9 </button>
-            <button value="+" onClick ={this.operation}> + </button>
-        </div>
-        <div>
-            <button value="4" onClick={this.getValue}> 4 </button>
-            <button value="5" onClick={this.getValue}> 5 </button>
-            <button value="6" onClick={this.getValue}> 6 </button>
-            <button value="-" onClick ={this.operation}> - </button>
-        </div>
-        <div>
-            <button value="1" onClick={this.getValue}> 1 </button>
-            <button value="2" onClick={this.getValue}> 2 </button>
-            <button value="3" onClick={this.getValue}> 3 </button>
-            <button value="*" onClick ={this.operation}>*</button>
-        </div>
-        <div>
-            <button value="0" onClick={this.getValue}> 0 </button>
-            <button onClick={this.clear}> C </button>
-            <button onClick={this.getResult}> = </button>
-            <button value="/" onClick ={this.operation}> / </button>
+          <div id="calculator">
+            <div className="top">
+              <button onClick={this.clear} className="clear"> C </button>
+              <div class="screen">
+                <Getvalues current={this.state.current} total={this.state.total} />
+              </div>
+            </div>
+            <div className="keys">
+              <div>
+                <button value="7" onClick={this.getValue}> 7 </button>
+                <button value="8" onClick={this.getValue}> 8 </button>
+                <button value="9" onClick={this.getValue}> 9 </button>
+                <button value="+" onClick={this.operation} className="operator"> + </button>
+              </div>
+              <div>
+                <button value="4" onClick={this.getValue}> 4 </button>
+                <button value="5" onClick={this.getValue}> 5 </button>
+                <button value="6" onClick={this.getValue}> 6 </button>
+                <button value="-" onClick={this.operation} className="operator"> - </button>
+              </div>
+              <div>
+                <button value="1" onClick={this.getValue}> 1 </button>
+                <button value="2" onClick={this.getValue}> 2 </button>
+                <button value="3" onClick={this.getValue}> 3 </button>
+                <button value="*" onClick={this.operation} className="operator"> * </button>
+              </div>
+              <div>
+                <button value="0" onClick={this.getValue}> 0 </button>
+                <button value="." onClick={this.getValue}> . </button>
+                <button onClick={this.getResult} class="eval"> = </button>
+                <button value="/" onClick ={this.operation} className="operator"> / </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
-
 
 export default App;
